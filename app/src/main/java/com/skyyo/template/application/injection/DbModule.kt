bde75480c485f9cfd89a7a5b3961798b39054c5c
Denis Rudenko <swifttropicbear@gmail.com>
2021-01-29 15:10:28 +0200

    updated hilt

--- app/src/main/java/com/skyyo/template/application/injection/DbModule.kt
+++ app/src/main/java/com/skyyo/template/application/injection/DbModule.kt
@@ -6,11 +6,11 @@ import com.skyyo.template.application.persistance.room.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object DbModule {
 
