4861c83b63688e81cc66f935efbc60f8d3dfb126
Denis Rudenko <swifttropicbear@gmail.com>
2021-02-22 14:52:06 +0200

    renamings

--- app/src/main/java/com/skyyo/template/application/repositories/auth/SocialSignInState.kt
+++
@@ -1,7 +0,0 @@
-package com.skyyo.template.application.repositories.auth
-
-sealed class SocialSignInState
-
-object SocialSignInSuccess : SocialSignInState()
-object SocialSignInSuccessFirstTime : SocialSignInState()
-object SocialSignInNetworkError : SocialSignInState()
