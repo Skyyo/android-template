d22630bcab4f4e9fe106554c50b17cf47779ab20
Denis Rudenko <swifttropicbear@gmail.com>
2021-02-26 15:09:50 +0200

    added DateTimeExtensions.kt to work with ISO-8601 & java8 time apis

---
+++ app/src/main/java/com/skyyo/template/extensions/DateTimeExtensions.kt
@@ -0,0 +1,30 @@
+package com.skyyo.template.extensions
+
+// date & time
+// fun localDateTime() = LocalDateTime.now().withNano(0).toString()
+//
+// fun dateTimeWithOffset() = OffsetDateTime.now().withNano(0).toString()
+//
+// fun String.toLocalDateTime() = LocalDateTime.parse(this).toString()
+//
+// fun String.toDateTimeWithOffset() = OffsetDateTime.parse(this).withNano(0).toString()
+
+// date
+// fun localDate() = LocalDate.now().toString()
+//
+// fun dateWithOffset() = OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE).toString()
+//
+// fun String.toLocalDate() =
+//    LocalDateTime.parse(this).format(DateTimeFormatter.ISO_LOCAL_DATE).toString()
+//
+// fun String.toOffsetDate() =
+//    OffsetDateTime.parse(this).format(DateTimeFormatter.ISO_OFFSET_DATE).toString()
+
+// time
+// fun localTime() = LocalTime.now().withNano(0).toString()
+//
+// fun timeWithOffset() = OffsetTime.now().withNano(0).toString()
+//
+// fun String.toLocalTime() = LocalDateTime.parse(this).toLocalTime().toString().toString()
+//
+// fun String.toOffsetTime() = OffsetDateTime.parse(this).toOffsetTime().toString().toString()
