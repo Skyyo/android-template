0907e95aadad5e9e6a63eaea322baac556821d51
Denis Rudenko <swifttropicbear@gmail.com>
2021-03-09 13:45:28 +0200

    bumped dependencies

--- app/src/main/java/com/skyyo/template/application/injection/AppModule.kt
+++ app/src/main/java/com/skyyo/template/application/injection/AppModule.kt
@@ -1,11 +1,5 @@
 package com.skyyo.template.application.injection
 
-import android.content.Context
-import androidx.datastore.createDataStore
-import androidx.datastore.preferences.createDataStore
-import com.skyyo.template.application.persistance.DataStoreManager
-import com.skyyo.template.application.persistance.PaymentMethodsProtoStoreManager
-import com.skyyo.template.protobuff.PaymentMethodsSerializer
 import com.skyyo.template.utils.eventDispatchers.UnauthorizedEventDispatcher
 import dagger.Module
 import dagger.Provides
@@ -16,16 +10,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun providePaymentMethodsProtoStoreManager(@ApplicationContext context: Context): PaymentMethodsProtoStoreManager =
-        PaymentMethodsProtoStoreManager(
-            context.createDataStore(
-                fileName = "PaymentMethodsSerializer.pb",
-                serializer = PaymentMethodsSerializer
-            )
-        )
 
     @Singleton
     @Provides
--- app/src/main/java/com/skyyo/template/application/persistance/PaymentMethodsProtoStoreManager.kt
+++ app/src/main/java/com/skyyo/template/application/persistance/PaymentMethodsProtoStoreManager.kt
@@ -1,17 +1,27 @@
 package com.skyyo.template.application.persistance
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
 import com.skyyo.template.application.models.local.PaymentMethod
 import com.skyyo.template.datastore.PaymentMethodProto
 import com.skyyo.template.datastore.PaymentMethods
+import com.skyyo.template.protobuff.PaymentMethodsSerializer
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 import javax.inject.Singleton
 
+val Context.paymentMethodsDataStore: DataStore<PaymentMethods> by dataStore(
+    fileName = "paymentMethods.pb",
+    serializer = PaymentMethodsSerializer
+)
+
 @Singleton
-class PaymentMethodsProtoStoreManager @Inject constructor(
-    private val paymentMethodProtoStore: DataStore<PaymentMethods>
-) {
+class PaymentMethodsProtoStoreManager @Inject constructor(@ApplicationContext context: Context) {
+
+    private val paymentMethodProtoStore = context.paymentMethodsDataStore
+
     // TODO use only proto generated model if possible
     suspend fun getPaymentMethods(): List<PaymentMethod>? =
         paymentMethodProtoStore.data.firstOrNull()?.paymentMethodList?.map { protoModel ->
